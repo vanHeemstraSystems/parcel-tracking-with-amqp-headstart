46172db5d865babdb1c03afec0565a942b35d36c
Willem van Heemstra <wvanheemstra@icloud.com>
2021-10-28 11:24:53 +0200

    Update

--- containers/app/amqp/server.js
+++ containers/app/amqp/server.js
@@ -13,7 +13,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("Connected to database"));
 const app = express();
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
 
 app.use(router);
 
